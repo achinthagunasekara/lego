2bed0364125bb3cc2faef49aabcfdf4a779de799
Achintha Gunasekara <achintha@me.com>
2018-08-04 17:29:25 +1000

    Working command module added. Example updated.

--- lego/builder_modules/command.py
+++ lego/builder_modules/command.py
@@ -3,6 +3,7 @@ Module to manage services on the system.
 """
 
 import logging
+from os import system
 from lego.common import LegoException, validate_attributes
 
 
@@ -22,10 +23,14 @@ def manage_commands(attributes):
     Raises:
         LegoException: Raises LegoException.
     """
-    logger = logging.getLogger('lego.builder_modules.services.manage_services')
+    logger = logging.getLogger('lego.builder_modules.commands.manage_commands')
     if not validate_attributes(provided_attributes=attributes.keys(),
                                supported_attributes=SUPPORTED_ATTRIBUTES,
                                compulsory_attributes=COMPULSORY_ATTRIBUTE):
         raise LegoException(message='Attribute validation failed')
     for each_command in attributes['commands']:
         logger.info("Command `%s` will run on the system", each_command)
+        return_code = system(each_command)
+        if return_code != 0:
+            raise LegoException(message="None 0 exit code `{0}` returned from "
+                                        "command `{1}`".format(return_code, each_command))
