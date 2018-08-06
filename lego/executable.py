a33d4d7b944931e5546eef1023cdfca46d915081
Achintha Gunasekara <achintha@me.com>
2018-08-06 16:21:19 +1000

    Now using argparse

--- lego/executable.py
+++ lego/executable.py
@@ -5,6 +5,7 @@ Executable for Lego configuration management tool.
 
 import sys
 import logging
+import argparse
 from lego.builder import Builder
 from lego.common import LegoException
 
@@ -12,35 +13,6 @@ from lego.common import LegoException
 SUPPORTED_COMMANDS = [
     'build'
 ]
-
-
-def lego_help():
-    """
-    Print help text for the executable.
-    Args:
-        None
-    Returns:
-        None
-    Raises:
-        None
-    """
-    help_text = ('Lego Configuration Management Tool\n'
-                 '\n'
-                 'You must use one of the following supported commands.\n'
-                 '\n'
-                 'Currently Supported Commands\n'
-                 '============================\n'
-                 '* build\n'
-                 '\n'
-                 'Options for Supported Commands\n'
-                 '==============================\n'
-                 '\n'
-                 'build\n'
-                 '-----\n'
-                 'build command must be followed by a builder file.\n'
-                 'Please refer to the tool documentation for builder file syntax\n')
-    print(help_text)  # pylint: disable=superfluous-parens
-
 
 def main():
     """
@@ -53,16 +25,37 @@ def main():
         None
     """
 
-    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', nargs='?', default=argparse.SUPPRESS,
+                        help="Action to preform. Currently supported actions "
+                        "are {0}".format(SUPPORTED_COMMANDS))
+    parser.add_argument('--action', dest='action', default=None,
+                        help="Action to preform. Currently supported actions "
+                        "are {0}".format(SUPPORTED_COMMANDS))
+    parser.add_argument('builder_file', nargs='?', default=argparse.SUPPRESS,
+                        help="Builder file to run")
+    parser.add_argument('--builder_file', dest='builder_file', default=None,
+                        help="Builder file to run")
+    parser.add_argument('--debug', default=False, action='store_true',
+                        help="Print debugging logs as well")
+    args = parser.parse_args()
+
+    print args
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('lego.executable.main')
 
-    if len(sys.argv) < 2 or sys.argv[1] not in SUPPORTED_COMMANDS:
-        lego_help()
+
+    if args.action not in SUPPORTED_COMMANDS:
+        parser.print_help()
         sys.exit(1)
 
-    if sys.argv[1] == 'build':
-        if len(sys.argv) < 3:
-            lego_help()
+    if args.action == 'build':
+        if not args.builder_file:
+            parser.print_help()
             sys.exit(1)
         try:
             builder = Builder(builder_file=sys.argv[2])
